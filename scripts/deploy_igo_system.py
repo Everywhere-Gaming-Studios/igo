943340ed4e120edb4735b477f5723a867caa8ab8
renatomrocha <renatomrocha93@gmail.com>
2022-05-04 17:57:07 +0100

    Added a few test components to testnet deploy script, was able to mint tokens to my wallet through public igo contract

---
+++ scripts/deploy_igo_system.py
@@ -0,0 +1,51 @@
+from brownie import FlattenedIgoToken, PublicIgo, IgoToken, PaymentCoin, accounts, network, config
+from scripts.utils import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS, payment_coin_symbol, payment_coin_name, investor_kyc
+from web3 import Web3
+
+account = get_account()
+
+
+def deploy_payment_coin():
+    return PaymentCoin.deploy(payment_coin_name, payment_coin_symbol, {"from": account})
+
+
+def deploy_public_igo():
+    active_network = network.show_active()
+    payment_coin = deploy_payment_coin()
+    print(f"Deploying on network: {active_network}")
+    public_igo = PublicIgo.deploy(config['igo_token_params']['price_numerator'], config['igo_token_params']['price_denominator'], payment_coin.address, {"from": account})
+    return public_igo, payment_coin
+
+
+def deploy_igo_token_factory():
+    public_igo, payment_coin = deploy_public_igo()
+    igo_token = IgoToken.deploy(config['igo_token_params']['name'], config['igo_token_params']['symbol'], public_igo.address , {"from": account})
+    public_igo.setIgoToken(igo_token.address)
+    print(f"Contract deployed to {igo_token.address}")
+    return igo_token, public_igo, payment_coin
+
+
+def main():
+    igo_token, public_igo, payment_coin = deploy_igo_token_factory()
+    account = get_account()
+
+    # Mint some TDAI to myself
+    payment_coin.mint(account, 10 * 10**18, {"from": account})
+
+    # Allow public igo to spend those DAI
+    payment_coin.approve(public_igo, 10* 10**18, {"from": account})
+
+    # Perform KYC
+    public_igo.performKyc(investor_kyc['email'], investor_kyc['country'])
+
+    # Buy tokens from igo
+    public_igo.buyTokens(10 * 10 ** 18, {"from": account})
+
+    # Change ownership to multisig
+    igo_token.transferOwnership(config['igo_token_params']['multisig_test_address'], {"from": account})
+    public_igo.transferOwnership(config['igo_token_params']['multisig_test_address'], {"from": account})
+
+
+
+
+
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,9 @@ from web3 import Web3
 DECIMALS = 8
 STARTING_PRICE = 200000000000
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+payment_coin_name = "Test DAI"
+payment_coin_symbol = "TDAI"
+investor_kyc = {'email': "renatomrocha93@gmail.com", 'country': 'Portugal'}
 
 
 def get_account():
@@ -14,3 +17,8 @@ def get_account():
         # Getting from env file
         account = accounts.add(config["wallets"]["from_key"])
     return account
+
+
+# def get_payment_coin():
+#     if network.show_active() == 'avax_test':
+#         return
